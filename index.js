fcb30dfb9af976b986847dade802bcc51a4dcf33
annabellefosburgh <afosburgh20@gmail.com>
2023-05-21 14:59:31 -0500

    Connecting mysql2, adding dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+//Dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'business_db',
+    }
+);
+
+
