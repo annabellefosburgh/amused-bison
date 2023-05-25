a652a3d786223f105973333f47c0b61be0f92fc6
annabellefosburgh <afosburgh20@gmail.com>
2023-05-25 14:49:42 -0500

    writing functions to perform choice actions

--- index.js
+++ index.js
@@ -27,3 +27,79 @@ inquirer
         },
         ])
 
+//Writing functions to show or append mysql data upon prompt choice
+
+function viewAllDepartments() {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, res) => {
+        throw new Error("Error!")
+    });
+}
+
+function viewAllRoles() {
+    const query = 'SELECT * FROM role';
+    connection.query(query, (err, res) => {
+        throw new Error("Error!")
+    });
+}
+
+function viewAllEmployees() {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        throw new Error("Error!")
+    });
+}
+
+function addADepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addDepartment',
+            message: 'Enter the name of the new department:'
+        }
+        .then((data) => {
+            connection.query('INSERT INTO department SET ?',
+                { name: data.newDepartment }, err => {
+                    throw new Error("Error!")
+                })
+        }),
+        viewAllDepartments()
+    ])
+}
+
+function addARole() {
+    const departments = connection.query()
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addRole',
+            message: 'Enter the name of the new role.',
+        },
+        {
+            type: 'list',
+            name: 'newRoleDept',
+            message: 'Choose which department this role will be in.',
+            choices: departments
+        }
+    ])
+}
+
+function addAnEmployee() {
+  const roles = connection.query()
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'Enter the employee first name.'
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'Enter the employee last name.'
+    },
+    
+  ])  
+}
+
+
+
